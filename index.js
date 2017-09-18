b7676d7c9f6c9d0beec66e2d903a5a2da9677bee
Peter van Ooy <pevanooy@gmail.com>
2017-09-18 23:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  
+}
+function destructivelyRemoveLastKitten(){
+
+}
+function destructivelyRemoveFirstKitten(){
+  
+}
+function appendKitten(name)(){
+  
+}
+function prependKitten(name)(){
+  
+}
+function removeLastKitten(){
+  
+}
+function  removeFirstKitten(){
+  
+}
